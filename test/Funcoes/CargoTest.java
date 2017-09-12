55f57ef5722d70b40542785c5d222a1ee95bb996
Mayco Douglas <may_co22@hotmail.com>
2017-09-12 09:33:18 -0300

    atualização 5

--- test/Funcoes/CargoTest.java
+++ test/Funcoes/CargoTest.java
@@ -5,10 +5,7 @@
  */
 package Funcoes;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -18,60 +15,7 @@ import static org.junit.Assert.*;
  */
 public class CargoTest {
     
-    public CargoTest() {
-    }
     
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-
-    /**
-     * Test of Cargo method, of class Cargo.
-     */
-    @Test
-    public void testCargo() {
-        System.out.println("Cargo");
-        Cargo instance = new Cargo();
-        instance.Cargo();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of cadastrarCargo method, of class Cargo.
-     */
-    @Test
-    public void testCadastrarCargo() {
-        System.out.println("cadastrarCargo");
-        Cargo instance = new Cargo();
-        instance.cadastrarCargo();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of pesquisaCargo method, of class Cargo.
-     */
-    @Test
-    public void testPesquisaCargo() {
-        System.out.println("pesquisaCargo");
-        Cargo instance = new Cargo();
-        instance.pesquisaCargo();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
 
     /**
      * Test of getId method, of class Cargo.
@@ -87,71 +31,5 @@ public class CargoTest {
         fail("The test case is a prototype.");
     }
 
-    /**
-     * Test of setId method, of class Cargo.
-     */
-    @Test
-    public void testSetId() {
-        System.out.println("setId");
-        int id = 0;
-        Cargo instance = new Cargo();
-        instance.setId(id);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getNome method, of class Cargo.
-     */
-    @Test
-    public void testGetNome() {
-        System.out.println("getNome");
-        Cargo instance = new Cargo();
-        String expResult = "";
-        String result = instance.getNome();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setNome method, of class Cargo.
-     */
-    @Test
-    public void testSetNome() {
-        System.out.println("setNome");
-        String nome = "";
-        Cargo instance = new Cargo();
-        instance.setNome(nome);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getTitulacaoMinima method, of class Cargo.
-     */
-    @Test
-    public void testGetTitulacaoMinima() {
-        System.out.println("getTitulacaoMinima");
-        Cargo instance = new Cargo();
-        String expResult = "";
-        String result = instance.getTitulacaoMinima();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setTitulacaoMinima method, of class Cargo.
-     */
-    @Test
-    public void testSetTitulacaoMinima() {
-        System.out.println("setTitulacaoMinima");
-        String titulacaoMinima = "";
-        Cargo instance = new Cargo();
-        instance.setTitulacaoMinima(titulacaoMinima);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
+   
 }
